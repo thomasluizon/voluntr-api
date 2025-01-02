f0a6c74891b4fd2a790fcc0165f24c738223d1c3
thomasluizon <thomaslrgregorio@gmail.com>
2025-01-02 20:04:37 -0300

    feat: crud quest

--- Voluntr/Voluntr.Domain/CommandHandlers/Project/Quest/AddQuestCommandHandler.cs
+++ Voluntr/Voluntr.Domain/CommandHandlers/Project/Quest/AddQuestCommandHandler.cs
@@ -1,16 +1,70 @@
 ﻿using Voluntr.Crosscutting.Domain.Commands.Handlers;
+using Voluntr.Crosscutting.Domain.Helpers.Extensions;
 using Voluntr.Crosscutting.Domain.MediatR;
 using Voluntr.Domain.Commands;
 using Voluntr.Domain.DataTransferObjects;
+using Voluntr.Domain.Enumerators;
+using Voluntr.Domain.Helpers.Constants;
+using Voluntr.Domain.Interfaces.Repositories;
+using Voluntr.Domain.Interfaces.Services;
+using Voluntr.Domain.Interfaces.UnitOfWork;
+using Voluntr.Domain.Models;
 
 namespace Voluntr.Domain.CommandHandlers
 {
     public class AddQuestCommandHandler(
-        IMediatorHandler mediator
+        IMediatorHandler mediator,
+        IClaimsService claimsService,
+        IProjectRepository projectRepository,
+        IQuestRepository questRepository,
+        IUnitOfWork unitOfWork
     ) : MediatorResponseCommandHandler<AddQuestCommand, CommandResponseDto>(mediator)
     {
         public override async Task<CommandResponseDto> AfterValidation(AddQuestCommand request)
         {
+            if (claimsService.GetCurrentUserType() != UserTypeEnum.Ngo.GetDescription())
+            {
+                NotifyError("O usuário informado não é uma ONG");
+                return null;
+            }
+
+            var project = await projectRepository.GetFirstByExpressionAsync(
+                x => x.Id == request.ProjectId,
+                x => x.Quests
+            );
+
+            if (project == null)
+            {
+                NotifyError("O projeto informado não foi encontrado");
+                return null;
+            }
+
+            if (project.Quests.FirstOrDefault(x => x.Name == request.Name) != null)
+            {
+                NotifyError("Já existe uma tarefa com o nome informado neste projeto");
+                return null;
+            }
+
+            var quest = new Quest
+            {
+                ProjectId = project.Id,
+                Name = request.Name,
+                Description = request.Description,
+                Reward = request.Reward,
+                DueDate = request.DueDate
+            };
+
+            await questRepository.InsertAsync(quest);
+
+            if (!HasNotification() && await unitOfWork.CommitAsync())
+            {
+                request.ExecutedSuccessfullyCommand = true;
+
+                return new CommandResponseDto { Id = quest.Id };
+            }
+            else
+                NotifyError(Values.Message.DefaultError);
+
             return null;
         }
     }
--- Voluntr/Voluntr.Domain/CommandHandlers/Project/Quest/UpdateQuestCommandHandler.cs
+++ Voluntr/Voluntr.Domain/CommandHandlers/Project/Quest/UpdateQuestCommandHandler.cs
@@ -1,16 +1,73 @@
 ﻿using Voluntr.Crosscutting.Domain.Commands.Handlers;
+using Voluntr.Crosscutting.Domain.Helpers.Extensions;
 using Voluntr.Crosscutting.Domain.MediatR;
 using Voluntr.Domain.Commands;
 using Voluntr.Domain.DataTransferObjects;
+using Voluntr.Domain.Enumerators;
+using Voluntr.Domain.Helpers.Constants;
+using Voluntr.Domain.Interfaces.Repositories;
+using Voluntr.Domain.Interfaces.Services;
+using Voluntr.Domain.Interfaces.UnitOfWork;
 
 namespace Voluntr.Domain.CommandHandlers
 {
     public class UpdateQuestCommandHandler(
-        IMediatorHandler mediator
+        IMediatorHandler mediator,
+        IClaimsService claimsService,
+        IProjectRepository projectRepository,
+        IQuestRepository questRepository,
+        IUnitOfWork unitOfWork
     ) : MediatorResponseCommandHandler<UpdateQuestCommand, CommandResponseDto>(mediator)
     {
         public override async Task<CommandResponseDto> AfterValidation(UpdateQuestCommand request)
         {
+            if (claimsService.GetCurrentUserType() != UserTypeEnum.Ngo.GetDescription())
+            {
+                NotifyError("O usuário informado não é uma ONG");
+                return null;
+            }
+
+            var project = await projectRepository.GetFirstByExpressionAsync(
+                x => x.Id == request.ProjectId,
+                x => x.Quests
+            );
+
+            if (project == null)
+            {
+                NotifyError("O projeto informado não foi encontrado");
+                return null;
+            }
+
+            if (project.Quests.FirstOrDefault(x => x.Name == request.Name && x.Id != request.Id) != null)
+            {
+                NotifyError("Já existe uma tarefa com o nome informado neste projeto");
+                return null;
+            }
+
+            var quest = project.Quests.FirstOrDefault(x => x.Id == request.Id);
+
+            if (quest == null)
+            {
+                NotifyError("A tarefa informada não foi encontrada");
+                return null;
+            }
+
+            quest.Name = request.Name;
+            quest.Description = request.Description;
+            quest.Reward = request.Reward;
+            quest.DueDate = request.DueDate;
+
+            await questRepository.UpdateAsync(quest);
+
+            if (!HasNotification() && await unitOfWork.CommitAsync())
+            {
+                request.ExecutedSuccessfullyCommand = true;
+
+                return new CommandResponseDto { Id = quest.Id };
+            }
+            else
+                NotifyError(Values.Message.DefaultError);
+
             return null;
         }
     }
