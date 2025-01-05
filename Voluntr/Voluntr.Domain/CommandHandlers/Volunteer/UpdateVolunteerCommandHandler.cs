4622f99f6ff76c512bfc48f26a9e3c7453bdc550
thomasluizon <thomaslrgregorio@gmail.com>
2025-01-05 19:03:17 -0300

    feat: update volunteer

--- Voluntr/Voluntr.Domain/CommandHandlers/Volunteer/UpdateVolunteerCommandHandler.cs
+++ Voluntr/Voluntr.Domain/CommandHandlers/Volunteer/UpdateVolunteerCommandHandler.cs
@@ -1,16 +1,73 @@
-﻿using Voluntr.Crosscutting.Domain.Commands.Handlers;
+﻿using AutoMapper;
+using Voluntr.Crosscutting.Domain.Commands.Handlers;
 using Voluntr.Crosscutting.Domain.MediatR;
 using Voluntr.Domain.Commands;
+using Voluntr.Domain.Helpers.Constants;
+using Voluntr.Domain.Interfaces.Repositories;
+using Voluntr.Domain.Interfaces.Services;
+using Voluntr.Domain.Interfaces.UnitOfWork;
+using Voluntr.Domain.Models;
 
 namespace Voluntr.Domain.CommandHandlers
 {
     public class UpdateVolunteerCommandHandler(
-        IMediatorHandler mediator
+        IMediatorHandler mediator,
+        IClaimsService claimsService,
+        IVolunteerRepository volunteerRepository,
+        IMapper mapper,
+        IAddressRepository addressRepository,
+        IUnitOfWork unitOfWork
     ) : MediatorCommandHandler<UpdateVolunteerCommand>(mediator)
     {
-        public override Task AfterValidation(UpdateVolunteerCommand request)
+        public override async Task AfterValidation(UpdateVolunteerCommand request)
         {
-            throw new NotImplementedException();
+            var userId = claimsService.GetCurrentUserId();
+
+            var volunteer = await volunteerRepository.GetFirstByExpressionAsync(
+                x => x.UserId == userId,
+                x => x.User
+            );
+
+            if (volunteer == null)
+            {
+                NotifyError("O voluntário informado não foi encontrado");
+                return;
+            }
+
+            if (await volunteerRepository.ExistsByExpressionAsync(
+                x => x.Nickname == request.Nickname &&
+                x.Id != volunteer.Id
+            ))
+            {
+                NotifyError("Já existe um voluntário com o apelido informado");
+                return;
+            }
+
+            if (await volunteerRepository.ExistsByExpressionAsync(
+                x => x.User.Email == request.Email &&
+                x.Id != volunteer.Id
+            ))
+            {
+                NotifyError("Já existe um voluntário com o email informado");
+                return;
+            }
+
+            volunteer.Nickname = request.Nickname;
+            volunteer.User.Name = request.Name;
+            volunteer.Surname = request.Surname;
+            volunteer.User.Email = request.Email;
+            volunteer.User.Phone = request.Phone;
+
+            var address = mapper.Map<Address>(request.Address);
+            address.UserId = volunteer.UserId;
+
+            await addressRepository.InsertAsync(address);
+            await volunteerRepository.UpdateAsync(volunteer);
+
+            if (!HasNotification() && await unitOfWork.CommitAsync())
+                request.ExecutedSuccessfullyCommand = true;
+            else
+                NotifyError(Values.Message.DefaultError);
         }
     }
 }
